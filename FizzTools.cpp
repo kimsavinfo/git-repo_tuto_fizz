c75f48817a330e185405328ea59049f0d7fd5c5e
Kim SAVAROCHE <kimsavinfo@gmail.com>
2021-08-18 13:59:39 +0200

    add fizz tools

---
+++ FizzTools.cpp
@@ -0,0 +1,13 @@
+//
+//  FizzTools.cpp
+//  RDGitRepoProjecct
+//
+//  Created by Kim SAVAROCHE on 23/07/2020.
+//  Copyright © 2020 Kim SAVAROCHE. All rights reserved.
+//
+
+#include "FizzTools.hpp"
+
+bool FizzTools::shouldFizz(const int number) {
+    return number % 3 == 0;
+}
---
+++ FizzTools.hpp
@@ -0,0 +1,20 @@
+//
+//  FizzTools.hpp
+//  RDGitRepoProjecct
+//
+//  Created by Kim SAVAROCHE on 23/07/2020.
+//  Copyright © 2020 Kim SAVAROCHE. All rights reserved.
+//
+
+#ifndef FizzTools_hpp
+#define FizzTools_hpp
+
+#include <stdio.h>
+#include <string>
+
+class FizzTools {
+public:
+    static bool shouldFizz(const int number);
+};
+
+#endif /* FizzTools_hpp */
